654c0f78468f13a9d4d4e47aeee4c7d9c41e1ca6
Tom Bracey <tombracey22@gmail.com>
2025-03-15 23:04:40 +0000

    finished function for flight price api

---
+++ src/utils/get_flight_price.py
@@ -0,0 +1,48 @@
+import os
+import requests
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
+
+def get_min_flight_price(departure_id, destination_id):
+    rapid_api_key = os.getenv("RAPIDAPI_KEY")
+
+    today = datetime.today().strftime('%Y-%m-%d')
+
+    url = "https://booking-com15.p.rapidapi.com/api/v1/flights/getMinPrice"
+
+    querystring = {
+        "fromId": f"{departure_id}.AIRPORT",
+        "toId": f"{destination_id}.AIRPORT",
+        "departDate": today,
+        "cabinClass": "ECONOMY",
+        "currency_code": "GBP",
+        "sort": "CHEAPEST"
+    }
+
+    headers = {
+        "x-rapidapi-key": rapid_api_key,
+        "x-rapidapi-host": "booking-com15.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    data = response.json()
+
+    cheapest_flight = None
+    if "data" in data:
+        for flight in data["data"]:
+            if cheapest_flight is None or flight["price"]["units"] < cheapest_flight["price"]["units"]:
+                cheapest_flight = flight
+
+    if cheapest_flight:
+        price_units = cheapest_flight["price"]["units"]
+        price_nanos = cheapest_flight["price"]["nanos"]
+        price = price_units + price_nanos / 1e9
+        date = cheapest_flight['departureDate']
+        return price, date
+    else:
+        return None
+
+if __name__ == "__main__":
+    print(get_min_flight_price("LON", "PAR"))
