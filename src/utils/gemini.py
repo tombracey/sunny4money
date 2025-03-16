d9fe238bddd6054c7b398f534fc8eb73541b7fab
Tom Bracey <tombracey22@gmail.com>
2025-03-16 19:08:49 +0000

    deleted old api key and hid new

--- src/utils/gemini.py
+++ src/utils/gemini.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
 import google.generativeai as genai
 
+load_dotenv()
+
 def gemini(prompt):
-    gemini_api_key = 'AIzaSyC_oY47M5uub372sCdDQN-pOX4FdTjGkzI'
+    gemini_api_key = os.getenv('GEMINI_API_KEY')
     genai.configure(api_key=gemini_api_key)
     model = genai.GenerativeModel("gemini-1.5-flash")
     request = model.generate_content(prompt)
