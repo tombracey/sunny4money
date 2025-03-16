2b48b4af1396c4aaba45b50651f5982d635f6218
Tom Bracey <tombracey22@gmail.com>
2025-03-16 19:59:19 +0000

    initialised django

---
+++ django_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from src.find_my_flight import find_my_flight
+
+def home(request):
+    if request.method == 'POST':
+        location = request.POST.get('location')
+        budget = float(request.POST.get('budget'))
+
+        result = find_my_flight(location, budget)
+
+        depart_from, city_name, pic_path, flight_price, flight_date, temperature, uk_temp_diff, ai_overview = result
+
+        return render(request, 'home.html', {
+            'city_name': city_name,
+            'flight_date': flight_date,
+        })
+    else:
+        return render(request, 'home.html')
