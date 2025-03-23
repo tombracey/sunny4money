e7285c8357e3a41a45cf489df00df6f51a9ed163
Tom Bracey <tombracey22@gmail.com>
2025-03-23 22:35:04 +0000

    added see the sea tickbox

--- django_app/views.py
+++ django_app/views.py
@@ -5,12 +5,19 @@ def home(request):
     if request.method == 'POST':
         location = request.POST.get('location')
         budget = float(request.POST.get('budget'))
-        result = find_my_flight(location, budget)
-        if result:
-            depart_from, city_name, pic_path, flight_price, flight_date, temperature, uk_temp_diff, ai_para_1, ai_para_2 = result
-        else:
-            depart_from = city_name = pic_path = flight_price = flight_date = temperature = uk_temp_diff = ai_para_1 = ai_para_2 = None
+        sea_the_sea = request.POST.get('sea') == 'on'
 
+        if sea_the_sea:
+            result = find_my_flight(location, budget, sea_the_sea)
+        else:
+            result = find_my_flight(location, budget)
+
+        if result:
+            depart_from, city_name, pic_path, flight_price, flight_date, temperature, uk_avg_temp, ai_para_1, ai_para_2 = result
+            uk_temp_diff = temperature - uk_avg_temp
+        else:
+            depart_from = city_name = pic_path = flight_price = flight_date = temperature = uk_avg_temp = ai_para_1 = ai_para_2 = None
+            uk_temp_diff = None
 
         return render(request, 'home.html', {
             'depart_from': depart_from,
@@ -19,9 +26,10 @@ def home(request):
             'flight_price': flight_price,
             'flight_date': flight_date,
             'temperature': temperature,
-            'uk_temp_diff': uk_temp_diff,
+            'uk_avg_temp': uk_avg_temp,
             'ai_para_1': ai_para_1,
             'ai_para_2': ai_para_2,
+            'uk_temp_diff': uk_temp_diff
         })
     else:
         return render(request, 'home.html')
