ba59bd4938d93fc3e95b8772b05eafb469cd1e36
Tom Bracey <tombracey22@gmail.com>
2025-03-23 22:28:59 +0000

    filters hotter countries before flights api requests

--- src/find_my_flight.py
+++ src/find_my_flight.py
@@ -8,35 +8,48 @@ from src.utils.find_nearest_airport import find_nearest_airport
 from src.utils.gemini import gemini
 
 destinations = {
-    "CDG": {"city": "Paris", "country": "France"},
-    "AMS": {"city": "Amsterdam", "country": "Netherlands"},
-    "FCO": {"city": "Rome", "country": "Italy"},
-    "MAD": {"city": "Madrid", "country": "Spain"},
-    "LIS": {"city": "Lisbon", "country": "Portugal"},
-    "ATH": {"city": "Athens", "country": "Greece"},
-    "BCN": {"city": "Barcelona", "country": "Spain"},
-    "BER": {"city": "Berlin", "country": "Germany"},
-    "MXP": {"city": "Milan", "country": "Italy"},
-    "VIE": {"city": "Vienna", "country": "Austria"},
-    "BRU": {"city": "Brussels", "country": "Belgium"},
-    "PRG": {"city": "Prague", "country": "Czech Republic"},
-    "BUD": {"city": "Budapest", "country": "Hungary"},
-    "ZRH": {"city": "Zurich", "country": "Switzerland"},
-    "CAI": {"city": "Cairo", "country": "Egypt"},
-    "CMN": {"city": "Casablanca", "country": "Morocco"},
-    "RAK": {"city": "Marrakech", "country": "Morocco"},
-    "TUN": {"city": "Tunis", "country": "Tunisia"},
-    "ALG": {"city": "Algiers", "country": "Algeria"},
-    "TIP": {"city": "Tripoli", "country": "Libya"},
-    "TNG": {"city": "Tangier", "country": "Morocco"},
-    "BEY": {"city": "Beirut", "country": "Lebanon"},
-    "AMM": {"city": "Amman", "country": "Jordan"},
-    "IST": {"city": "Istanbul", "country": "Turkey"},
-    "DOH": {"city": "Doha", "country": "Qatar"},
-    "BAH": {"city": "Manama", "country": "Bahrain"}
+    "CDG": {"city": "Paris", "country": "France", "beach": False},
+    "AMS": {"city": "Amsterdam", "country": "Netherlands", "beach": False},
+    "FCO": {"city": "Rome", "country": "Italy", "beach": True},
+    "MAD": {"city": "Madrid", "country": "Spain", "beach": False},
+    "LIS": {"city": "Lisbon", "country": "Portugal", "beach": True},
+    "ATH": {"city": "Athens", "country": "Greece", "beach": True},
+    "BCN": {"city": "Barcelona", "country": "Spain", "beach": True},
+    "BER": {"city": "Berlin", "country": "Germany", "beach": False},
+    "MXP": {"city": "Milan", "country": "Italy", "beach": False},
+    "VIE": {"city": "Vienna", "country": "Austria", "beach": False},
+    "BRU": {"city": "Brussels", "country": "Belgium", "beach": False},
+    "PRG": {"city": "Prague", "country": "Czech Republic", "beach": False},
+    "BUD": {"city": "Budapest", "country": "Hungary", "beach": False},
+    "ZRH": {"city": "Zurich", "country": "Switzerland", "beach": False},
+    "CAI": {"city": "Cairo", "country": "Egypt", "beach": False},
+    "CMN": {"city": "Casablanca", "country": "Morocco", "beach": True},
+    "RAK": {"city": "Marrakech", "country": "Morocco", "beach": False},
+    "TUN": {"city": "Tunis", "country": "Tunisia", "beach": True},
+    "ALG": {"city": "Algiers", "country": "Algeria", "beach": True},
+    "TIP": {"city": "Tripoli", "country": "Libya", "beach": True},
+    "TNG": {"city": "Tangier", "country": "Morocco", "beach": True},
+    "BEY": {"city": "Beirut", "country": "Lebanon", "beach": True},
+    "AMM": {"city": "Amman", "country": "Jordan", "beach": True},
+    "IST": {"city": "Istanbul", "country": "Turkey", "beach": True},
+    "DOH": {"city": "Doha", "country": "Qatar", "beach": True},
+    "BAH": {"city": "Manama", "country": "Bahrain", "beach": True},
+    "NCE": {"city": "Nice", "country": "France", "beach": True},
+    "JTR": {"city": "Santorini", "country": "Greece", "beach": True},
+    "FAO": {"city": "Albufeira", "country": "Portugal", "beach": True},
+    "CTA": {"city": "Catania", "country": "Italy", "beach": True},
+    "AGP": {"city": "Malaga", "country": "Spain", "beach": True},
+    "DBV": {"city": "Dubrovnik", "country": "Croatia", "beach": True},
+    "NAP": {"city": "Naples", "country": "Italy", "beach": True},
+    "TLN": {"city": "Toulon", "country": "France", "beach": True},
+    "SSH": {"city": "Sharm El Sheikh", "country": "Egypt", "beach": True},
+    "HRG": {"city": "Hurghada", "country": "Egypt", "beach": True},
+    "DJE": {"city": "Djerba", "country": "Tunisia", "beach": True},
+    "ESU": {"city": "Essaouira", "country": "Morocco", "beach": True},
+    "AGA": {"city": "Agadir", "country": "Morocco", "beach": True}
 }
 
-def find_my_flight(location: str, budget):
+def find_my_flight(location: str, budget, see_the_sea=False):
     departure_code = find_nearest_airport(location)[0]
 
     today = datetime.today().strftime('%Y-%m-%d')
@@ -47,23 +60,26 @@ def find_my_flight(location: str, budget):
     dir_path = f'./data/{today}'
     csv_path = dir_path + f'/{departure_code}.csv'
 
-    if not os.path.exists(csv_path):
-        headers = ["City", "Price", "Date"]
-        with open (csv_path, 'w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(headers)
-            for destination, details in destinations.items():
-                flight = get_min_flight_price(departure_code, destination)
-                if flight:
-                    city = details["city"]
-                    price = flight[0]
-                    date = flight[1]
-                    writer.writerow([city, price, date])
+    headers = ["City", "Price", "Date", "Beach"]
+    with open (csv_path, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        for destination, details in destinations.items():
+            flight = get_min_flight_price(departure_code, destination)
+            if flight:
+                city = details["city"]
+                beach = details["beach"]
+                price = flight[0]
+                date = flight[1]
+                writer.writerow([city, price, date, beach])
     temps_df = pd.read_csv(f'{dir_path}/temps.csv')
     flights_df = pd.read_csv(csv_path)
     merged_df = pd.merge(flights_df, temps_df, on="City", how="inner")
-    london_temp = temps_df.loc[temps_df['City'] == 'London', 'Temperature (°C)'].values[0]
-    filtered_df = merged_df[(merged_df['Price'] <= budget) & (merged_df['Temperature (°C)'] > london_temp)]
+    uk_avg_temp = int(temps_df.loc[temps_df['City'] == 'United Kingdom', 'Temperature (°C)'].values[0])
+    filtered_df = merged_df[merged_df['Price'] <= budget]
+
+    if see_the_sea:
+        filtered_df = filtered_df[filtered_df['Beach'] == True]
 
     if filtered_df.empty:
         return None
@@ -72,18 +88,17 @@ def find_my_flight(location: str, budget):
     city_name = hottest_city_row['City']
     temperature = int(hottest_city_row['Temperature (°C)'])
     flight_price = float(hottest_city_row['Price'])
-    uk_temp_diff = temperature - int(temps_df[temps_df['City'] == "London"]['Temperature (°C)'].values[0])
 
     unformatted_date = hottest_city_row['Date']
     flight_date = datetime.strptime(unformatted_date, "%Y-%m-%d").strftime("%d %B %Y")
 
-    ai_overview = gemini(f'Write 2 couple paragraphs split by <<PARAGRAPH>> to someone who is going to {city_name} with plane tickets costing £{flight_price}. It is {uk_temp_diff} hotter than here in the UK. Talk about what a great destination it is, how they are getting a lot of sun for value and the unique attractions/history/culture. No preamble.')
+    ai_overview = gemini(f'Write 2 couple paragraphs split by <<PARAGRAPH>> to someone who is going to {city_name} with plane tickets costing £{flight_price}. It is {temperature-uk_avg_temp} hotter than here in the UK. Talk about what a great destination it is, how they are getting a lot of sun for value and the unique attractions/history/culture. No preamble.')
     ai_para_1 = ai_overview[0]
     ai_para_2 = ai_overview[1]
 
     depart_from = find_nearest_airport(location)[1]
     pic_path = f'media/images/{city_name}.jpg'
 
-    return depart_from, city_name, pic_path, flight_price, flight_date, temperature, uk_temp_diff, ai_para_1, ai_para_2
+    return depart_from, city_name, pic_path, flight_price, flight_date, temperature, uk_avg_temp, ai_para_1, ai_para_2
 
 print(find_my_flight("Hanwell", 45))
--- src/weather.py
+++ src/weather.py
@@ -14,22 +14,21 @@ def get_weather(city):
     data = response.json()
     
     if response.status_code == 200:
-        max_temp = 0
+        max_kelvin_temp = 0
         
         for forecast in data['list']:
             kelvin_temp = forecast['main']['temp_max']
             
-            if max_temp is None or kelvin_temp > max_temp:
-                max_temp = kelvin_temp
+            if max_kelvin_temp is None or kelvin_temp > max_kelvin_temp:
+                max_kelvin_temp = kelvin_temp
         
-        temp = round(max_temp - 273)
+        temp = round(max_kelvin_temp - 273)
         return temp
     else:
         return None
 
 
 locations = {
-    "UK": {"city": "London", "country": "UK"},
     "CDG": {"city": "Paris", "country": "France"},
     "AMS": {"city": "Amsterdam", "country": "Netherlands"},
     "FCO": {"city": "Rome", "country": "Italy"},
@@ -55,10 +54,22 @@ locations = {
     "AMM": {"city": "Amman", "country": "Jordan"},
     "IST": {"city": "Istanbul", "country": "Turkey"},
     "DOH": {"city": "Doha", "country": "Qatar"},
-    "BAH": {"city": "Manama", "country": "Bahrain"}
+    "BAH": {"city": "Manama", "country": "Bahrain"},
+    "NCE": {"city": "Nice", "country": "France"},
+    "JTR": {"city": "Santorini", "country": "Greece"},
+    "FAO": {"city": "Albufeira", "country": "Portugal"},
+    "CTA": {"city": "Catania", "country": "Italy"},
+    "AGP": {"city": "Malaga", "country": "Spain"},
+    "DBV": {"city": "Dubrovnik", "country": "Croatia"},
+    "NAP": {"city": "Naples", "country": "Italy"},
+    "TLN": {"city": "Toulon", "country": "France"},
+    "SSH": {"city": "Sharm El Sheikh", "country": "Egypt"},
+    "HRG": {"city": "Hurghada", "country": "Egypt"},
+    "DJE": {"city": "Djerba", "country": "Tunisia"},
+    "ESU": {"city": "Essaouira", "country": "Morocco"},
+    "AGA": {"city": "Agadir", "country": "Morocco"}
 }
 
-headers = ["City", "Temperature (°C)"]
 
 today = datetime.today().strftime('%Y-%m-%d')
 new_directory = f'./data/{today}'
@@ -66,12 +77,17 @@ os.makedirs(new_directory, exist_ok=True)
 
 weather_file_path = new_directory + '/temps.csv'
 with open(weather_file_path, 'w', newline='') as f:
+    headers = ["City", "Temperature (°C)"]
     writer = csv.writer(f)
     writer.writerow(headers)
+
+    uk = "United Kingdom"
+    uk_temp = get_weather(uk)
+    writer.writerow([uk, uk_temp])
 
     for code, details in locations.items():
         city = details["city"]
         temp = get_weather(city)
         
-        if temp:
+        if temp > uk_temp:
             writer.writerow([city, temp])
