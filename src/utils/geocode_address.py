7f3591324e465f21ffc061b5de13ac475c4f88a1
Tom Bracey <tombracey22@gmail.com>
2025-03-23 15:28:06 +0000

    stopped defaulting to US addresses with UK place names

--- src/utils/geocode_address.py
+++ src/utils/geocode_address.py
@@ -3,10 +3,13 @@ import boto3
 client = boto3.client('location')
 
 def geocode_address(address):
+        location = address + ", United Kingdom"
         response = client.search_place_index_for_text(
             IndexName='MyPlaceIndex',
-            Text=address
+            Text=location
         )
         lon, lat = response['Results'][0]['Place']['Geometry']['Point']
         coordinates = (lat, lon)
         return coordinates
+
+print(geocode_address("Hanwell"))
