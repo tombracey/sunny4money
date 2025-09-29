f1a9a440a14521e96fd615cf2c61bf0be51178a7
Tom Bracey <tombracey22@gmail.com>
2025-09-29 16:39:02 +0100

    had to upgrade gemini model

--- src/utils/gemini.py
+++ src/utils/gemini.py
@@ -7,7 +7,7 @@ load_dotenv()
 def gemini(prompt):
     gemini_api_key = os.getenv('GEMINI_API_KEY')
     genai.configure(api_key=gemini_api_key)
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    model = genai.GenerativeModel("gemini-2.0-flash")
     request = model.generate_content(prompt)
     response = request.to_dict()["candidates"][0]["content"]["parts"][0]["text"]
     paragraphs = response.split('<<PARAGRAPH>>')
@@ -21,6 +21,6 @@ def gemini(prompt):
     else:
       return response
     
-ai_overview = gemini(f'Write 2 couple paragraphs split by <<PARAGRAPH>> to someone who is going to Paris with plane tickets costing £111. It is 8 degrees hotter than here in the UK. Talk about what a great destination it is, how they are getting a lot of sun for value and the unique attractions/history/culture. No preamble.')
+ai_overview = gemini(f'Write 2 paragraphs split by <<PARAGRAPH>> to someone who is going to Paris with plane tickets costing £111. It is 8 degrees hotter than here in the UK. Talk about what a great destination it is, how they are getting a lot of sun for value and the unique attractions/history/culture. No preamble.')
 
-print(ai_overview)
+# print(ai_overview)
